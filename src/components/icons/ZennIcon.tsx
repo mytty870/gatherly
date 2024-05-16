4d8502b77e9bbcfa306e9806ac66a92cd7df7cda
mytty870 <otani11ace@icloud.com>
2024-05-16 17:31:06 +0900

    add icon

---
+++ src/components/icons/ZennIcon.tsx
@@ -0,0 +1,17 @@
+import { IconBase, IconBaseProps } from './IconBase'
+
+export const ZennIcon = (props: IconBaseProps) => {
+  return (
+    <IconBase
+      viewBox="0 0 80 88"
+      width="18"
+      height="18"
+      title="zenn-icon"
+      fill="#3EA8FF"
+      {...props}
+    >
+      <path d="M2.4,83.3h17c0.9,0,1.7-0.5,2.2-1.2L68.4,5.2C69,4.2,68.3,3,67.1,3H51c-0.8,0-1.5,0.4-1.9,1.1L1.6,81.9 C1.3,82.5,1.7,83.3,2.4,83.3z"></path>
+      <path d="M61,82.1l22.1-35.5c0.7-1.1-0.1-2.5-1.4-2.5H65.7c-0.6,0-1.2,0.3-1.5,0.8L41.5,81.2c-0.6,0.9,0.1,2.1,1.2,2.1 h16.3C59.8,83.3,60.6,82.9,61,82.1z"></path>
+    </IconBase>
+  )
+}
