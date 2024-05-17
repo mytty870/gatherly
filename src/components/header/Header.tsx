6cd3e6efb76f159f13a0d3a011fc77205c06cf93
mytty870 <otani11ace@icloud.com>
2024-05-17 21:20:05 +0900

    fix header design

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,8 @@ export const Header = () => {
   return (
     <>
       <header className="relative border-b border-[#eaeaea] bg-white">
-        <div className="mx-6 flex min-h-12 items-center justify-between sm:min-h-14">
+        {/* <div className="mx-6 flex min-h-12 items-center justify-between sm:min-h-14"> */}
+        <div className="mx-4 flex min-h-12 items-center justify-between sm:mx-6 sm:min-h-16">
           <h1 className="text-2xl font-medium">
             <Link href="/">Gatherly</Link>
           </h1>
