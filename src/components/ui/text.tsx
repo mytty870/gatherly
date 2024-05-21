26f2472ad2ff0827c45eac1e80a820a68ba2f144
mytty870 <otani11ace@icloud.com>
2024-05-21 17:15:00 +0900

    add Text Component

---
+++ src/components/ui/text.tsx
@@ -0,0 +1,69 @@
+import { cn } from '@/lib/utils'
+import { VariantProps, cva } from 'class-variance-authority'
+import React from 'react'
+
+const textVariants = cva('', {
+  variants: {
+    variantColor: {
+      black: 'text-black',
+      slateGray: 'text-[#65717b]',
+    },
+    size: {
+      xs: 'text-[0.78rem]',
+      sm: 'text-[0.85rem]',
+      md: 'text-[0.95rem]',
+      base: 'text-base',
+    },
+    fontWeight: {
+      normal: 'font-normal',
+      medium: 'font-medium',
+      semibold: 'font-semibold',
+      bold: 'font-bold',
+    },
+  },
+  defaultVariants: {
+    variantColor: 'black',
+    size: 'base',
+    fontWeight: 'normal',
+  },
+})
+
+export interface TextProps
+  extends React.HTMLAttributes<HTMLParagraphElement | HTMLSpanElement>,
+    VariantProps<typeof textVariants> {
+  as?: 'p' | 'span'
+}
+
+const Text = React.forwardRef<
+  HTMLParagraphElement | HTMLSpanElement,
+  TextProps
+>(
+  (
+    {
+      children,
+      className,
+      variantColor,
+      size,
+      fontWeight,
+      as: Component = 'p',
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <Component
+        className={cn(
+          textVariants({ variantColor, size, fontWeight }),
+          className,
+        )}
+        ref={ref as React.Ref<HTMLParagraphElement & HTMLSpanElement>}
+        {...props}
+      >
+        {children}
+      </Component>
+    )
+  },
+)
+Text.displayName = 'Text'
+
+export { Text, textVariants }
