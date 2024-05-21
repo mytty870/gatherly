9ee2fce03ad242ad0b1dccdd848b9aa7433b189f
mytty870 <otani11ace@icloud.com>
2024-05-21 17:16:32 +0900

    add Input Component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,15 +3,29 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = 'text', autoComplete = 'off', ...props }, ref) => {
+  (
+    {
+      className,
+      type = 'text',
+      autoComplete = 'off',
+      hasError = false,
+      ...props
+    },
+    ref,
+  ) => {
     return (
       <input
         type={type}
         className={cn(
-          'flex w-full bg-[#f5f9fc] focus:border-[#8d9298] focus:outline-none focus:shadow-[0_0_0_2.5px_#d8dadf] text-base rounded-lg border border-[#d6e3ed] px-2.5 py-2 text-base',
+          'flex w-full focus:outline-none text-base rounded-lg border px-2.5 py-2 text-base',
+          hasError
+            ? 'bg-white border-destructive focus:shadow-[0_0_0_2.5px_#FECACA]'
+            : 'bg-[#f5f9fc] border-[#d6e3ed] focus:border-[#8d9298] focus:shadow-[0_0_0_2.5px_#d8dadf]',
           className,
         )}
         autoComplete={autoComplete}
