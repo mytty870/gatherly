569528e1454a0ba6acf03d04bc599e70da05b6cb
mytty870 <otani11ace@icloud.com>
2024-05-22 21:13:38 +0900

    modyfy next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -5,10 +5,10 @@ declare module 'next-auth' {
   interface Session {
     user: {
       id: string
+      userName?: string | null
       name?: string | null
       email?: string | null
       image?: string | null
-      userName?: string | null
     }
   }
 }
