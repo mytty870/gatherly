923566d0e1365909b1ad5724c152360c5730f84b
mytty870 <otani11ace@icloud.com>
2024-05-24 17:43:39 +0900

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+import { features } from 'process'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(tsx|ts|jsx|js)', '../src/**/*.mdx'],
+  features: { experimentalRSC: true },
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+}
+export default config
