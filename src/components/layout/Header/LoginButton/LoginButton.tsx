ba9be5104af98cc3d1bb5d199f99accd62dbd283
mytty870 <otani11ace@icloud.com>
2024-06-04 20:14:41 +0900

    add Login Dialog

---
+++ src/components/layout/Header/LoginButton/LoginButton.tsx
@@ -0,0 +1,61 @@
+'use client'
+import { GithubIcon, GoogleIcon } from '@/components/icons'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog/Dialog'
+import { Button } from '@/components/ui/button/Button'
+import { signIn } from 'next-auth/react'
+
+export const LoginButton = () => {
+  const handleGoogleAuthenticationClick = () => {
+    signIn('google', { callbackUrl: 'https://localhost:8080' })
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size="sm">Sign In</Button>
+      </DialogTrigger>
+      <DialogContent
+        onOpenAutoFocus={e => e.preventDefault()}
+        onCloseAutoFocus={e => e.preventDefault()}
+        className="p-10 sm:max-w-[425px]"
+      >
+        <DialogHeader className="gap-3">
+          <DialogTitle className="text-4xl">Gatherly</DialogTitle>
+          <DialogDescription>
+            Gatherly に登録すれば、あなたの
+            Zenn、Qiita、Note、しずかなインターネットなどの記事を Gatherly
+            に集約することができます。
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4">
+          <Button
+            variant="basic"
+            radius="full"
+            size="lg"
+            fullWidth
+            startContent={<GoogleIcon />}
+            onClick={handleGoogleAuthenticationClick}
+          >
+            Google アカウントでログイン
+          </Button>
+          <Button
+            variant="basic"
+            radius="full"
+            size="lg"
+            fullWidth
+            startContent={<GithubIcon width="1.4rem" height="1.4rem" />}
+          >
+            Github アカウントでログイン
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
