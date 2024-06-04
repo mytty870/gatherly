6a34832225bef71377efdab2a9a51bd09e4ac66e
mytty870 <otani11ace@icloud.com>
2024-06-04 20:08:36 +0900

    add SettingsTabs

---
+++ src/app/(private)/settings/account/_components/SettingsTabs/SettingsTabs.tsx
@@ -0,0 +1,39 @@
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from '@/components/ui/tabs/Tabs'
+import { Profile } from '@/types'
+import { BasicSettingsTabsContent } from './BasicSettingsTabsContent'
+import { ArticleUrlSettingsTabsContent } from './ArticleUrlSettingsTabsContent'
+
+export type SettingsTabsProps = {
+  profile: Profile
+}
+
+export const SettingsTabs = ({ profile }: SettingsTabsProps) => {
+  const defaultTabName = 'basic-settings'
+
+  return (
+    <Tabs defaultValue={defaultTabName} className="mt-5 w-[88vw] max-w-[600px]">
+      <TabsList className="mb-10">
+        <TabsTrigger value="basic-settings">基本設定</TabsTrigger>
+        <TabsTrigger value="article-settings">記事設定</TabsTrigger>
+      </TabsList>
+      <TabsContent value="basic-settings" className="flex flex-col gap-6">
+        <BasicSettingsTabsContent
+          displayName={profile.displayName}
+          bio={profile.bio}
+        />
+      </TabsContent>
+      <TabsContent value="article-settings" className="flex flex-col gap-6">
+        <ArticleUrlSettingsTabsContent
+          zennUserName={'aaaa'}
+          quiitaUserName={profile.quiitaUserName}
+          noteUserName={profile.noteUserName}
+        />
+      </TabsContent>
+    </Tabs>
+  )
+}
