c6ffc1ec59ff285075c2fed8ae837bdca5ed7594
mytty870 <otani11ace@icloud.com>
2024-06-04 20:12:52 +0900

    add prifile fixture

---
+++ src/test/fixtures/profile.ts
@@ -0,0 +1,18 @@
+import { Profile } from '@/types'
+
+export const profileFixture: Profile = {
+  id: '1',
+  displayName: 'John Doe',
+  avatarUrl: '',
+  userName: 'johndoe',
+  bio: '私の名前は johndoe です。gorigori 大学出身です。',
+  zennUserName: null,
+  quiitaUserName: null,
+  noteUserName: null,
+  sizuUserName: null,
+  githubUserName: null,
+  twiterUserName: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  userId: '1',
+}
