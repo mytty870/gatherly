584d44a503367538143751bd98e84e21857e8271
mytty870 <otani11ace@icloud.com>
2024-06-14 10:43:34 +0900

    replace ServiceUrlSettingForm with individual service forms in ArticleUrlSettingsTabsContent

--- src/app/(private)/settings/account/_components/SettingsTabs/ArticleUrlSettingsTabsContent/ArticleUrlSettingsTabsContent.tsx
+++ src/app/(private)/settings/account/_components/SettingsTabs/ArticleUrlSettingsTabsContent/ArticleUrlSettingsTabsContent.tsx
@@ -1,52 +1,37 @@
 'use client'
 import { Card } from '@/components/ui/card/Card'
 import { Profile } from '@/types'
-import { ServiceUrlSettingForm } from './ServiceUrlSettingForm'
-import {
-  NoteUrlSettingFormTypes,
-  QuiitaUrlSettingFormTypes,
-  noteUrlSettingFormSchema,
-  quiitaUrlSettingFormSchema,
-  ZennUrlSettingFormTypes,
-  zennUrlSettingFormSchema,
-} from './ServiceUrlSettingForm/schema'
+import { NoteUrlSettingForm } from './NoteUrlSettingForm'
+import { QuiitaUrlSettingForm } from './QuiitaUrlSettingForm'
+import { ZennUrlSettingForm } from './ZennUrlSettingForm'
+import { SizuUrlSettingForm } from './SizuUrlSettingForm'
 
 type ArticleUrlSettingsTabsContentProps = {
   zennUserName: Profile['zennUserName']
   quiitaUserName: Profile['quiitaUserName']
   noteUserName: Profile['noteUserName']
+  sizuUserName: Profile['sizuUserName']
 }
 
 export const ArticleUrlSettingsTabsContent = ({
   zennUserName,
   quiitaUserName,
   noteUserName,
+  sizuUserName,
 }: ArticleUrlSettingsTabsContentProps) => {
   return (
     <>
       <Card>
-        <ServiceUrlSettingForm<ZennUrlSettingFormTypes>
-          service="Zenn"
-          userNameKey="zennUserName"
-          userName={zennUserName ?? ''}
-          schema={zennUrlSettingFormSchema}
-        />
+        <ZennUrlSettingForm zennUserName={zennUserName} />
       </Card>
       <Card>
-        <ServiceUrlSettingForm<QuiitaUrlSettingFormTypes>
-          service="Quiita"
-          userNameKey="quiitaUserName"
-          userName={quiitaUserName ?? ''}
-          schema={quiitaUrlSettingFormSchema}
-        />
+        <QuiitaUrlSettingForm quiitaUserName={quiitaUserName} />
       </Card>
       <Card>
-        <ServiceUrlSettingForm<NoteUrlSettingFormTypes>
-          service="Note"
-          userNameKey="noteUserName"
-          userName={noteUserName ?? ''}
-          schema={noteUrlSettingFormSchema}
-        />
+        <NoteUrlSettingForm noteUserName={noteUserName} />
+      </Card>
+      <Card>
+        <SizuUrlSettingForm sizuUserName={sizuUserName} />
       </Card>
     </>
   )
