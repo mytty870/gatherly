2b2348443715ae2bc166897d7b2de9a6e40f9d02
mytty870 <otani11ace@icloud.com>
2024-06-16 23:48:14 +0900

    refactor: Change profile data passing to individual props in UserProfile

--- src/app/(private)/[userName]/_components/userProfile/UserProfile.tsx
+++ src/app/(private)/[userName]/_components/userProfile/UserProfile.tsx
@@ -6,28 +6,34 @@ import { Profile } from '@/types'
 import Image from 'next/image'
 
 type UserProfileProps = {
-  profile: Profile
+  avatarUrl: Profile['avatarUrl']
+  bio: Profile['bio']
+  displayName: Profile['displayName']
 }
 
-export const UserProfile = ({ profile }: UserProfileProps) => {
+export const UserProfile = ({
+  avatarUrl,
+  bio,
+  displayName,
+}: UserProfileProps) => {
   return (
     <div className="flex min-h-[160px] w-[80vw] max-w-[900px] flex-col gap-3 rounded-lg border p-4">
       <div className="flex items-center gap-3">
         <Avatar size="sm">
           <Image
-            src={profile.avatarUrl}
+            src={avatarUrl ?? ''}
             alt="Avatar Icon"
             width={500}
             height={500}
           />
         </Avatar>
         <Heading level={2} size="ml" fontWeight="normal" align="left">
-          {profile.displayName}
+          {displayName}
         </Heading>
       </div>
       <div>
         <Text variantColor="charcoalGray" size="md">
-          {profile.bio ? profile.bio : '自己紹介文が入力されていません'}
+          {bio ? bio : '自己紹介文が入力されていません'}
         </Text>
       </div>
     </div>
