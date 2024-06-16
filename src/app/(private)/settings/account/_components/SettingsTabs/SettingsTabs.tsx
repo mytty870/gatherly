67092feb59eee7f18bb493d4fe7173cc2c391584
mytty870 <otani11ace@icloud.com>
2024-06-16 17:52:30 +0900

    Add avatarUrl prop to BasicSettingsTabsContent in SettingsTabs

--- src/app/(private)/settings/account/_components/SettingsTabs/SettingsTabs.tsx
+++ src/app/(private)/settings/account/_components/SettingsTabs/SettingsTabs.tsx
@@ -16,15 +16,19 @@ export const SettingsTabs = ({ profile }: SettingsTabsProps) => {
   const defaultTabName = 'basic-settings'
 
   return (
-    <Tabs defaultValue={defaultTabName} className="mt-5 w-[88vw] max-w-[600px]">
+    <Tabs
+      defaultValue={defaultTabName}
+      className="mb-10 mt-5 w-[88vw] max-w-[600px]"
+    >
       <TabsList className="mb-10">
         <TabsTrigger value="basic-settings">基本設定</TabsTrigger>
         <TabsTrigger value="article-settings">記事設定</TabsTrigger>
       </TabsList>
-      <TabsContent value="basic-settings" className="mb-10 flex flex-col gap-6">
+      <TabsContent value="basic-settings" className="flex flex-col gap-6">
         <BasicSettingsTabsContent
           displayName={profile?.displayName ?? ''}
           bio={profile?.bio ?? ''}
+          avatarUrl={profile?.avatarUrl ?? ''}
         />
       </TabsContent>
       <TabsContent value="article-settings" className="flex flex-col gap-6">
