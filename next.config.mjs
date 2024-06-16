8e83f15cc14f81689f4623c1f6b86efbf211bad3
mytty870 <otani11ace@icloud.com>
2024-06-16 21:24:42 +0900

    chore: update Next.js configuration for image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   experimental: {
     typedRoutes: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pzbvignsswqmjgadqmov.supabase.co',
+        pathname: '/storage/v1/object/public/gatherly-avatar/**',
+      }
+    ]
   }
 };
 
