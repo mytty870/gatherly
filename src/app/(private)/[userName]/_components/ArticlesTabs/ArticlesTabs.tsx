5e0e250d6d0e5df1f63446ec95f3fe9be8670346
mytty870 <otani11ace@icloud.com>
2024-06-17 21:33:56 +0900

    feat: Add ArticlesTabs component to display articles from multiple sources
    - Created `ArticlesTabs` component to display articles from Zenn, Sizu, Qiita, and Note.
    - Integrated tabs with corresponding icons and articles for each platform.
    - Implemented UI using the Tabs component for easy navigation between different article sources.

---
+++ src/app/(private)/[userName]/_components/ArticlesTabs/ArticlesTabs.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { Articles } from './Articles'
+import { ZennFullIcon, SizuIcon, NoteIcon } from '@/components/icons'
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui/tabs/Tabs'
+import { Article } from '@/services/types'
+import { Text } from '@/components/ui/text/Text'
+
+type ArticleTabsProps = {
+  zennArticles: Article[]
+  sizuArticles: Article[]
+  qiitaArticles: Article[]
+  noteArticles: Article[]
+}
+
+export const ArticlesTabs = ({
+  zennArticles,
+  sizuArticles,
+  qiitaArticles,
+  noteArticles,
+}: ArticleTabsProps) => {
+  return (
+    <>
+      <Tabs
+        defaultValue="zenn"
+        className="mt-5 w-[88vw] max-w-[400px] md:w-[692px] md:max-w-[832px] lg:w-[832px]"
+      >
+        <TabsList className="mb-10">
+          <TabsTrigger value="zenn">Zenn</TabsTrigger>
+          <TabsTrigger value="sizu">Sizu</TabsTrigger>
+          <TabsTrigger value="qiita">Qiita</TabsTrigger>
+          <TabsTrigger value="note">Note</TabsTrigger>
+        </TabsList>
+        <TabsContent value="zenn">
+          <Articles articles={zennArticles} mediaIcon={<ZennFullIcon />} />
+        </TabsContent>
+        <TabsContent value="sizu">
+          <Articles
+            articles={sizuArticles}
+            mediaIcon={
+              <>
+                <SizuIcon />
+                <Text as="span" size="sm" fontWeight="medium">
+                  しずかなインターネット
+                </Text>
+              </>
+            }
+          />
+        </TabsContent>
+        <TabsContent value="qiita">
+          <Articles articles={qiitaArticles} mediaIcon={<ZennFullIcon />} />
+        </TabsContent>
+        <TabsContent value="note">
+          <Articles articles={noteArticles} mediaIcon={<NoteIcon />} />
+        </TabsContent>
+      </Tabs>
+    </>
+  )
+}
