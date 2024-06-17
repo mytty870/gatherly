1e4eec0c7fa581c030aa134de52dd8e19f30ebb2
mytty870 <otani11ace@icloud.com>
2024-06-17 21:28:21 +0900

    feat: Add getSizuArticles function to fetch and parse Zenn RSS feed

---
+++ src/services/getSizuArticles/getSizuArticles.ts
@@ -0,0 +1,81 @@
+import { Parser } from 'xml2js'
+import { Article } from '../types'
+
+export const getSizuRSSData = async (userName: string) => {
+  const SIZU_RSS_URL = `https://sizu.me/${userName}/rss`
+
+  try {
+    const response = await fetch(SIZU_RSS_URL, {
+      next: {
+        revalidate: 3600,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch Sizu RSS')
+    }
+
+    const text = await response.text()
+
+    if (!text.startsWith('<?xml')) {
+      throw new Error('Invalid RSS feed format')
+    }
+
+    return text
+  } catch (error) {
+    if (process.env.NODE_ENV === 'development') {
+      console.error(error)
+    }
+
+    throw error
+  }
+}
+
+const formatDate = (date: Date): string => {
+  const year = date.getFullYear()
+  const month = `0${date.getMonth() + 1}`.slice(-2)
+  const day = `0${date.getDate()}`.slice(-2)
+  return `${year}/${month}/${day}`
+}
+
+export const getSizuArticles = async (userName: string): Promise<Article[]> => {
+  try {
+    const sizuRSSData = await getSizuRSSData(userName)
+
+    const parser = new Parser({ explicitArray: false, trim: true })
+
+    const xmlToJson = await parser.parseStringPromise(sizuRSSData)
+
+    let array = xmlToJson.rss.channel.item
+
+    if (!Array.isArray(array)) {
+      array = [array]
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const articles: Article[] = array.map((article: any) => {
+      const publishedDate = new Date(article.pubDate)
+
+      const isNewly =
+        new Date().getTime() - publishedDate.getTime() <=
+        30 * 24 * 60 * 60 * 1000
+
+      return {
+        title: article.title,
+        description: article.description,
+        url: article.link,
+        publishedDate: formatDate(publishedDate),
+        creatorName: article['dc:creator'],
+        isNewly,
+      }
+    })
+
+    return articles
+  } catch (error) {
+    if (process.env.NODE_ENV === 'development') {
+      console.error(error)
+    }
+
+    return []
+  }
+}
