66d2d8132f50ae7297db1100f755b0c0008a819a
mytty870 <otani11ace@icloud.com>
2024-06-18 16:25:58 +0900

    refactor: wrap registration form in a Card component for better UI consistency

--- src/app/(public)/signin/welcome/page.tsx
+++ src/app/(public)/signin/welcome/page.tsx
@@ -1,9 +1,9 @@
 import { redirect } from 'next/navigation'
-// import { getServerSession } from '../../../../lib/auth'
 import { getServerSession } from '@/lib/auth'
 import React from 'react'
 import { Heading } from '@/components/ui/heading/Heading'
 import { UserRegistrationForm } from './_components/userRegistrationForm/UserRegistrationForm'
+import { Card } from '@/components/ui/card/Card'
 
 export default async function WelcomePage() {
   const session = await getServerSession()
@@ -12,34 +12,23 @@ export default async function WelcomePage() {
     redirect('/signin')
   }
 
-  /**
-   * 仮でルートにリダイレクトするように設定
-   */
   if (session.user?.userName) {
-    redirect('/')
+    redirect(`/${session.user?.userName}`)
   }
 
   return (
-    <div className="py-10">
-      <div className="mx-auto grid w-full max-w-[428px] gap-4 px-6">
+    <div className="mt-10 px-4 py-10">
+      <Card
+        padding="lg"
+        shadow="lg"
+        fullWidth
+        className="mx-auto max-w-[428px] gap-4"
+      >
         <Heading level={1} fontWeight="normal" align="center">
           アカウントを作成します
         </Heading>
-        {/* <div>
-          <label className='text-[0.85rem] font-bold mb-2' htmlFor='username-field'>
-            ユーザー名
-            <Text as="span" size="xs" variantColor="slateGray" className='ml-3'>登録後は変更することができません</Text>
-          </label>
-          <div className='flex items-center gap-2'>
-            <label className='text-[#65717b] tracking-[.02em]' htmlFor='username-field'>
-              gatherly.com/
-            </label>
-            <Input placeholder='example' id="username-field" type="text" required aria-required="true" />
-
-          </div>
-        </div> */}
         <UserRegistrationForm />
-      </div>
+      </Card>
     </div>
   )
 }
