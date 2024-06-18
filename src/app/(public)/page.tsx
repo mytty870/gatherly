9d193a9875d80e6b67291538d61ad8a08d21dcd6
mytty870 <otani11ace@icloud.com>
2024-06-18 15:07:40 +0900

    refactor: simplify Home page and update Header import path

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,22 +1,11 @@
-import { ArticleSection } from '@/components/articleSection/ArticleSection'
-import { Header } from '@/components/header/Header'
-// import { getServerSession } from '@/lib/auth'
-// import { fetchZennPosts } from '@/lib/data'
+import { Header } from '@/components/layout/Header/Header'
 
 export default async function Home() {
-  // const data = await fetchZennPosts()
-  // const session = await getServerSession()
-  // console.log('aa',session)
-
   return (
     <>
       <Header />
       <main className="h-screen">
-        <div>
-          <div className="flex justify-center">
-            <ArticleSection />
-          </div>
-        </div>
+        <div>Home Page</div>
       </main>
     </>
   )
