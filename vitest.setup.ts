9b29a956f2b14fe5ab06ae01f61140bfccb55f3d
shota_imai <84974943+mytty870@users.noreply.github.com>
2024-06-21 16:12:54 +0900

    Homeコンポーネントのレンダリングとリダイレクト機能のテスト追加 (#13)
    * chore: add next-router-mock to devDependencies for routing tests
    
    * test: add unit tests for NotFound component
    
    * test: Add rendering and redirection tests for Home component

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,24 @@
 import '@testing-library/jest-dom'
+
+import { vi } from 'vitest'
+
+// next/font/google モジュールのモック設定
+// https://github.com/vercel/next.js/issues/59701
+// TypeError: Inter is not a function というエラーが発生したため、上記で解消
+vi.mock('next/font/google', () => ({
+  Inter: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+  Noto_Serif_JP: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+  Zen_Kaku_Gothic_New: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+}))
